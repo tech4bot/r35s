1adad9b6f040aa17676db354789d987d6180171e
tech4bot <hackaround4fun@gmail.com>
2023-10-14 21:30:11 +0200

    add fix for volume buttons

--- AmberELEC/fix_audio.sh
+++ AmberELEC/fix_audio.sh
@@ -10,6 +10,8 @@ case $response in
     mkdir -p /var/lib/alsa/
     amixer sset 'Playback Path' SPK_HP
     alsactl store
+    ln -s /dev/input/event3 /dev/input/by-path/platform-rg351-keys-event
+    systemctl restart volume.service
     exit
   ;;
 esac
--- AmberELEC/fix_audio_without_confirmation.sh
+++ AmberELEC/fix_audio_without_confirmation.sh
@@ -3,4 +3,6 @@
 mkdir -p /var/lib/alsa/
 amixer sset 'Playback Path' SPK_HP
 alsactl store
+ln -s /dev/input/event3 /dev/input/by-path/platform-rg351-keys-event
+systemctl restart volume.service
 exit
